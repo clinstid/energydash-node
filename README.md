8037f7cbf1cbf6d6f6dc8f57fa7576c38425fc7d
Chris Linstid <clinstid@gmail.com>
2017-09-10 21:56:10 -0400

    Initial commit with something that kinda works

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # energydash-node
-Energy monitor API in Node
+Energy monitor API in Node/restify
---
+++ api/db/index.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+    host: 'yoda',
+    user: 'energydash',
+    database: 'energydash',
+    password: 'energydash',
+    max: 20
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params)
+}
